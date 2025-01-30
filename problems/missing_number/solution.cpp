581ac1865e5a87184065d5206ee3834f18919b97
maniksangra2006 <maniksangra2006@gmail.com>
2025-01-30 16:18:42 +0000

    Sync LeetCode submission - Missing Number (cpp)

---
+++ problems/missing_number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+         int sum1=0;
+         int n=nums.size();
+         sum1=(n*(n+1))/2;
+         int sum2=0;
+         for(int i=0;i<nums.size();i++){
+            sum2+=nums[i];
+         }
+         return sum1-sum2;
+    }
+         }; 
