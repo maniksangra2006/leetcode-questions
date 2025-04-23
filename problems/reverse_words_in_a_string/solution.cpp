d692361a5f0c69b343eeaa2412fd0a3523ba2002
maniksangra2006 <maniksangra2006@gmail.com>
2025-04-23 05:03:19 +0000

    Sync LeetCode submission - Reverse Words in a String (cpp)

--- problems/reverse_words_in_a_string/solution.cpp
+++ problems/reverse_words_in_a_string/solution.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     string reverseWords(string s) {
-      reverse(s.begin(), s.end());
-              int n = s.size();
-                      int left = 0;
-                              int right = 0;
-                                      int i = 0;
-                                              while (i < n) {
-                                                          while (i < n && s[i] == ' ')
-                                                                          i++;
-                                                                                      if (i == n)
-                                                                                                      break;
-                                                                                                                  while (i < n && s[i] != ' ') {
-                                                                                                                                  s[right++] = s[i++];
-                                                                                                                                              }
-                                                                                                                                                          reverse(s.begin() + left, s.begin() + right);
-                                                                                                                                                                      s[right++] = ' ';
-                                                                                                                                                                                  left = right;
-                                                                                                                                                                                              i++;
-                                                                                                                                                                                                      }
-                                                                                                                                                                                                              s.resize(right - 1);
-                                                                                                                                                                                                                      return s;  
+       int n=s.size();
+       string ans="";
+       reverse(s.begin(),s.end());
+       for(int i=0;i<n;i++){
+        string word="";
+        while(i<n && s[i]!=' '){
+            word+=s[i];
+            i++;
+        }
+        reverse(word.begin(),word.end());
+        if(word.size()>0){
+            ans+=" "+word;
+        }
+               }
+               return ans.substr(1);
     }
 };
