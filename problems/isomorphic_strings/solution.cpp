b0e30ca2a164840a927479aa23878671cfbd60c1
maniksangra2006 <maniksangra2006@gmail.com>
2025-04-28 15:51:24 +0000

    Sync LeetCode submission - Isomorphic Strings (cpp)

--- problems/isomorphic_strings/solution.cpp
+++ problems/isomorphic_strings/solution.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>mp1;
-        unordered_map<char,char>mp2;
-
-        int n=s.size();
-        for(int i=0;i<n;i++){
-        char ch1=s[i];
-        char ch2=t[i];
-        if(mp1.find(ch1)!=mp1.end() && mp1[ch1]!=ch2 || mp2.find(ch2)!=mp2.end() && mp2[ch2]!=ch1)
-            
-        
-        {
-            return false;
-        }
-        mp1[ch1]=ch2;
-        mp2[ch2]=ch1;
-        }
-       return true;
+        vector<int> indexS(200, 0); // Stores index of characters in string s
+                vector<int> indexT(200, 0); // Stores index of characters in string t
+                        
+                                int len = s.length(); // Get the length of both strings
+                                        
+                                                if(len != t.length()) { // If the lengths of the two strings are different, they can't be isomorphic
+                                                            return false;
+                                                                    }
+                                                                            
+                                                                                    for(int i = 0; i < len; i++) { // Iterate through each character of the strings
+                                                                                                if(indexS[s[i]] != indexT[t[i]]) { // Check if the index of the current character in string s is different from the index of the corresponding character in string t
+                                                                                                                return false; // If different, strings are not isomorphic
+                                                                                                                            }
+                                                                                                                                        
+                                                                                                                                                    indexS[s[i]] = i + 1; // updating position of current character
+                                                                                                                                                                indexT[t[i]] = i + 1;
+                                                                                                                                                                        }
+                                                                                                                                                                                
+                                                                                                                                                                                        return true;
     }
 };
