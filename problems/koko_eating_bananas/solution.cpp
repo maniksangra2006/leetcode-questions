840b4b7ad8d237c5f19a6fc6055620ff0f13ad4e
maniksangra2006 <maniksangra2006@gmail.com>
2025-05-20 04:20:43 +0000

    Sync LeetCode submission - Koko Eating Bananas (cpp)

--- problems/koko_eating_bananas/solution.cpp
+++ problems/koko_eating_bananas/solution.cpp
@@ -1,30 +1,30 @@
 class Solution {
-public:
-bool canEatAll(vector<int> &piles,int mid,int h){
-    int actualHours=0;
-    for(int &x : piles){
-       
-            actualHours+=x/mid;
+public: 
+bool canEatAll(vector<int>& piles ,int mid,int h){
+      long long actualHours = 0;
+    for (int i=0;i<piles.size();i++) {
+    int x = piles[i]; 
+        actualHours += x/mid;  
         if(x%mid!=0){
             actualHours++;
         }
-
     }
-    return actualHours<=h;
-}
-  int minEatingSpeed(vector<int> &piles ,int h){
-     int n=piles.size();
-     int low=1;
-     int high=*max_element(begin(piles),end(piles));
-     while(low<high){
-        int mid=(low+high)/2;
-        if(canEatAll(piles,mid,h)){
-            high=mid;
+    return actualHours <= h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        int ans=high;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(canEatAll(piles,mid,h)){
+                ans=mid;
+                high=mid-1;
+            }
+          else{
+                    low=mid+1;
+                }
         }
-        else{
-            low=mid+1;
-        }
-     }
-     return low;
+        return ans;
     }
 };
