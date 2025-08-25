ff584b180ec600132671d9bb073372f808561d1f
maniksangra2006 <maniksangra2006@gmail.com>
2025-08-25 06:37:55 +0000

    Sync LeetCode submission - Linked List Cycle (cpp)

--- problems/linked_list_cycle/solution.cpp
+++ problems/linked_list_cycle/solution.cpp
@@ -9,15 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* fast=head;
+        ListNode* fast=head; ;
         ListNode* slow=head;
-        while(fast!=nullptr && fast->next!=nullptr){
-            fast =fast->next->next;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
             slow=slow->next;
-            if(slow==fast){
-                return true ;
+            if(fast==slow){
+                return true;
             }
         }
         return false;
-    }
+}
 };
