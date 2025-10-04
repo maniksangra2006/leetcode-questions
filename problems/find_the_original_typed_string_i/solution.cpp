a3d4f21f6fc2bba1e76dc40047ff571db9c5c389
maniksangra2006 <maniksangra2006@gmail.com>
2025-10-04 16:23:15 +0000

    Sync LeetCode submission - Find the Original Typed String I (cpp)

---
+++ problems/find_the_original_typed_string_i/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int n =word.size();
+        int count=n;
+        for(int i=1;i<n;i++){
+            if(word[i]!=word[i-1]){
+                count--;
+            }
+        }
+        return count;
+    }
+};
