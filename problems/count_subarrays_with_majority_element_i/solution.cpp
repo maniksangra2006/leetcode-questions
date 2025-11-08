2ee27743e8a6f72abfd69a3fdeb75b26f62fd1d1
maniksangra2006 <maniksangra2006@gmail.com>
2025-11-08 15:26:23 +0000

    Sync LeetCode submission - Count Subarrays With Majority Element I (cpp)

---
+++ problems/count_subarrays_with_majority_element_i/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n =nums.size();
+        int ans=0;
+        vector<int> num = nums;
+         for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = i; j < n; j++) {
+                if (num[j] == target) cnt++;
+                int len = j - i + 1;
+                if (cnt * 2 > len) ans++;
+            }
+        }
+        return ans;
+    }
+};
