2a241bb8bbf048440221d741baab94c2c4095c23
maniksangra2006 <maniksangra2006@gmail.com>
2025-11-09 03:24:26 +0000

    Sync LeetCode submission - Count Complete Tree Nodes (cpp)

---
+++ problems/count_complete_tree_nodes/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private: 
+    int cnt=0;
+    void solve(TreeNode* node){
+    if(node==NULL) return;
+    cnt++;
+    solve(node->left);
+    solve(node->right);
+}
+public: 
+    int countNodes(TreeNode* root) {
+        solve(root);
+        return cnt;
+    }
+};
