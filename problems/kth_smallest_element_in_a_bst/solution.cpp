3f082a3a6dfbc6847c740684a0d48f7327804329
maniksangra2006 <maniksangra2006@gmail.com>
2025-11-22 15:49:56 +0000

    Sync LeetCode submission - Kth Smallest Element in a BST (cpp)

--- problems/kth_smallest_element_in_a_bst/solution.cpp
+++ problems/kth_smallest_element_in_a_bst/solution.cpp
@@ -10,18 +10,20 @@
  * };
  */
 class Solution {
-public:
+public:  
 int solve(TreeNode* root,int &i,int k){
-if(!root) return -1;
-int left=solve(root->left,i,k);
-if(left!=-1) return left;
-i++;
-if(i==k) return root->val;
-return solve(root->right,i,k);
-}
-    int kthSmallest(TreeNode* root, int k) {
-        int i=0;
-        int ans=solve(root,i,k);
-        return ans;
+    if(!root) return -1;
+    int left=solve(root->left,i,k);
+    if(left!=-1) return left;
+    i++;
+    if(i==k) return root->val;
+    return solve(root->right,i,k);
     }
-};
+        int kthSmallest(TreeNode* root, int k) {
+                int i=0;
+                        int ans=solve(root,i,k);
+                                return ans;
+                                    }
+                                    };
+
+    
