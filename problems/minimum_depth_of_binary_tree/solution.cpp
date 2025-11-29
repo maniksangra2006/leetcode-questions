b6aa5e6a79710718e97b1ec914eb93f07b288d7d
maniksangra2006 <maniksangra2006@gmail.com>
2025-11-29 17:23:36 +0000

    Sync LeetCode submission - Minimum Depth of Binary Tree (cpp)

---
+++ problems/minimum_depth_of_binary_tree/solution.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root) return NULL;
+        int left=minDepth(root->left);
+        int right=minDepth(root->right);
+
+        if(root->left==NULL) return right+1;
+        if(root->right==NULL) return left+1;
+        return min(left,right)+1;
+
+    }
+};
