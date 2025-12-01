ae5f42cea5f06c0c422013e5050e39ad3186d779
maniksangra2006 <maniksangra2006@gmail.com>
2025-12-01 18:52:15 +0000

    Sync LeetCode submission - Reverse Vowels of a String (cpp)

--- problems/reverse_vowels_of_a_string/solution.cpp
+++ problems/reverse_vowels_of_a_string/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        string word = s;
+         string word = s;
         int start = 0;
         int end = s.length() - 1;
         string vowels = "aeiouAEIOU";
