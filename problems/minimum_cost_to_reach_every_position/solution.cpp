1d05b7b1c34dde72217d68cae6f74c4be5e4c461
maniksangra2006 <maniksangra2006@gmail.com>
2025-12-19 10:38:57 +0000

    Sync LeetCode submission - Minimum Cost to Reach Every Position (cpp)

---
+++ problems/minimum_cost_to_reach_every_position/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> minCosts(vector<int>& cost) {
+         for(int i=0;i<cost.size();i++){
+              if(i!=0){
+                if(cost[i]>cost[i-1])
+                 cost[i]=cost[i-1];
+            }
+           }
+        return cost;
+    }
+};
