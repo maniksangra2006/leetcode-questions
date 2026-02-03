088b9ed4add9fe3b1cd9cb03327e12dcb98c2268
maniksangra2006 <maniksangra2006@gmail.com>
2026-02-03 20:51:25 +0000

    Sync LeetCode submission - Assign Cookies (cpp)

---
+++ problems/assign_cookies/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0;
+        int j=0;
+        int cnt=0;
+        while(i<s.size() && j<g.size() ){
+            if(s[i]>=g[j]){
+                cnt++;
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+
+        }
+        return cnt;
+    }
+};
