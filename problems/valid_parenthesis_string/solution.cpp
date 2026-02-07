2b1f26b7fe7e1574e198033718124904412cf423
maniksangra2006 <maniksangra2006@gmail.com>
2026-02-07 19:13:38 +0000

    Sync LeetCode submission - Valid Parenthesis String (cpp)

---
+++ problems/valid_parenthesis_string/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int lmin=0;
+        int lmax=0;
+        for(char c:s){
+            if(c=='('){
+                lmin++;
+                lmax++;
+            }
+            else if (c==')'){
+                lmin--;
+                lmax--;
+            }
+            else{
+                lmin--;
+                lmax++;
+            }
+            if(lmax<0) return false;
+            if(lmin<0) lmin=0;
+        }
+        return lmin==0;
+    }
+};
