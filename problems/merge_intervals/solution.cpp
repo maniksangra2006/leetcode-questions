59486250f8fc49010f6d3277cee8064cdef8e6f5
maniksangra2006 <maniksangra2006@gmail.com>
2026-02-11 09:46:55 +0000

    Sync LeetCode submission - Merge Intervals (cpp)

--- problems/merge_intervals/solution.cpp
+++ problems/merge_intervals/solution.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>> &  intervals) {
-         sort(intervals.begin(),intervals.end());
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
-
-        int previous_l = intervals[0][0];
-        int previous_r = intervals[0][1];
-
-        for(auto interval:intervals){
-            int l = interval[0],r = interval[1];
-            if(l <= previous_r && r >= previous_r){ // merge
-                previous_r = r;
+        sort(intervals.begin(),intervals.end());
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if( intervals[i][0]<=ans.back()[1]){
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
-            else if(l > previous_r && r > previous_r){ // do not merge
-                ans.push_back({previous_l,previous_r});
-                previous_l = l;
-                previous_r = r;
+            else{
+                ans.push_back(intervals[i]);
             }
         }
-        ans.push_back({previous_l,previous_r});
-
         return ans;
     }
 };
