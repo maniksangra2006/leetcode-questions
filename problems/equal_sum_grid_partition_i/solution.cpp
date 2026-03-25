f3e33d92afad053436e20c7e52fe32b8cb63242a
maniksangra2006 <maniksangra2006@gmail.com>
2026-03-25 02:59:09 +0000

    Sync LeetCode submission - Equal Sum Grid Partition I (cpp)

---
+++ problems/equal_sum_grid_partition_i/solution.cpp
@@ -0,0 +1,30 @@
+ class Solution {
+public:
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+       int n =grid.size();
+       int m =grid[0].size();
+       long long total=0;
+       for(auto &row: grid){
+        for(int x: row){
+            total+=x;
+        }
+       }
+       if(total%2!=0) return false;
+       long long target=total/2;
+       long long sum=0;
+       for(int i=0;i<n-1;i++){
+        for(int j=0;j<m;j++){
+            sum+=grid[i][j];
+        }
+        if(sum==target) return true;
+       }
+      sum=0;
+       for(int i=0;i<m-1;i++){
+        for(int j=0;j<n;j++){
+            sum+=grid[j][i];
+         }
+         if(sum==target) return true;
+       }
+         return false;
+     }
+};
