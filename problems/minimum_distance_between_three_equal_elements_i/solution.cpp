ae38fa60886d982236dca2c2183dec506fa8f8f0
maniksangra2006 <maniksangra2006@gmail.com>
2026-04-13 06:15:37 +0000

    Sync LeetCode submission - Minimum Distance Between Three Equal Elements I (cpp)

--- problems/minimum_distance_between_three_equal_elements_i/solution.cpp
+++ problems/minimum_distance_between_three_equal_elements_i/solution.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-         int n = nums.size();
-        int minDist = INT_MAX;
-         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[i] == nums[j] && nums[j] == nums[k]) {
-                        int dist = abs(i - j) + abs(j - k) + abs(k - i);
-                        minDist = min(minDist, dist);
-                    }
+        int n =nums.size();
+      
+        if(n<3) return -1;
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++){
+         for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                if(nums[i]==nums[j]&& nums[j]==nums[k]){
+                    int dis=abs(i-j)+abs(j-k)+abs(k-i);
+                    mini=min(dis,mini);
+                   
                 }
             }
+         }
+         
         }
-         return (minDist == INT_MAX) ? -1 : minDist;
+        return mini==INT_MAX? -1 : mini;
     }
 };
