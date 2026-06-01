558c82b9aa25221700f10fbc664184a5c6dc1ceb
maniksangra2006 <maniksangra2006@gmail.com>
2026-06-01 06:01:40 +0000

    Sync LeetCode submission - Minimum Cost of Buying Candies With Discount (cpp)

---
+++ problems/minimum_cost_of_buying_candies_with_discount/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.rbegin(),cost.rend());
+        int n =cost.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if((i+1)%3==0) continue;
+
+            ans+=cost[i];
+        }
+        return ans;
+    }
+};
