d4712f00d4f5ee6c707794474ab4d012a697ef1a
maniksangra2006 <maniksangra2006@gmail.com>
2026-06-01 19:41:48 +0000

    Sync LeetCode submission - Longest Common Prefix (cpp)

--- problems/longest_common_prefix/solution.cpp
+++ problems/longest_common_prefix/solution.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans="";
+       string ans="";
         for(int i=0;i<strs[0].size();i++){
             for(int j=0;j<strs.size()-1;j++){
-                if(strs[j][i]!=strs[j+1][i])  return ans;
+                if(strs[j][i]!=strs[j+1][i]) return ans;
             }
-        
-        ans+=strs[0][i];
-    
-        
+            ans+=strs[0][i];
         }
         return ans;
     }
-
 };
