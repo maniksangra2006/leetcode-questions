befa1868fc8f1e3b64ad76b17948f03fdfcf818c
maniksangra2006 <maniksangra2006@gmail.com>
2026-06-09 09:28:08 +0000

    Sync LeetCode submission - Sqrt(x) (cpp)

--- problems/sqrt(x)/solution.cpp
+++ problems/sqrt(x)/solution.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        int low=1;
-        int high=x;
-        int result=1;
-        while(low<=high){
-            long long mid=low+(high-low)/2;
-            long long val=mid*mid;
-            if(val<=x){
-                 
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+long long int binary(int n){
+    int s=0;
+    int e =n;
+    long long int ans=-1;
+    long long int mid=s+(e-s)/2;
+    while(s<=e){
+        long long sq=mid*mid;
+        if(sq==n) return mid;
+        if(sq<=n){
+            ans=mid;
+            s=mid+1;
         }
-        return high;
+        else {
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
+    int mySqrt(int x) {
+       return binary(x);
+
     }
 };
