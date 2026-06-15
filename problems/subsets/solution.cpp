73f62e87da7d06c2b03e3726dc33c671ba42aa99
maniksangra2006 <maniksangra2006@gmail.com>
2026-06-15 11:52:08 +0000

    Sync LeetCode submission - Subsets (cpp)

--- problems/subsets/solution.cpp
+++ problems/subsets/solution.cpp
@@ -1,17 +1,19 @@
- class Solution {
+class Solution {
 public:
+vector<vector<int>> res;
+vector<int> ans;
+void backtrack(vector<int> & nums,int strt){
+    res.push_back(ans);
+    for(int i=strt;i<nums.size();i++){
+    ans.push_back(nums[i]);
+        backtrack(nums,i+1);
+        ans.pop_back();
+    }
+}
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        int n = nums.size();
-        for (int i = 0; i < (1 << n); i++) {
-            vector<int> subset;
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) > 0) {
-                    subset.push_back(nums[j]);
-                }
-            }
-            result.push_back(subset);
-        }
-        return result;
+        res.clear();
+        ans.clear();
+        backtrack(nums,0);
+        return res;
     }
 };
