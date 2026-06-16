fe39ec96040bdf240ef34a48cedf653d019218b0
maniksangra2006 <maniksangra2006@gmail.com>
2026-06-16 00:43:22 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (cpp)

--- problems/letter_combinations_of_a_phone_number/solution.cpp
+++ problems/letter_combinations_of_a_phone_number/solution.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-vector<string>result;
-void solve(int idx,string &digits,string &temp,unordered_map<char,string>&mp){
-      if(idx>=digits.size()){
-        result.push_back(temp);
+void solve(string digits,string output,vector<string>& ans,int idx,string mapping[]){
+    if(idx>=digits.size()) {
+        ans.push_back(output);
         return;
-      }
-      char ch=digits[idx];
-      string str=mp[ch];
-      for(int i=0;i<str.size();i++){
-        temp.push_back(str[i]);
-        solve(idx+1,digits,temp,mp);
-        temp.pop_back();
-      }
+    }
+    int num=digits[idx]-'0';
+    string val=mapping[num];
+    for(int i=0;i<val.size();i++){
+        output.push_back(val[i]);
+        solve(digits,output,ans,idx+1,mapping);
+        output.pop_back();
+        
+    }
 }
     vector<string> letterCombinations(string digits) {
-        if (digits.size()==0) {
-        return{} ;
-        }
- 
-        unordered_map<char,string>mp;
-        mp['2']="abc";
-        mp['3']="def";
-        mp['4']="ghi";
-        mp['5']="jkl";
-        mp['6']="mno";
-        mp['7']="pqrs";
-        mp['8']="tuv";
-        mp['9']="wxyz";
-        string temp="";
-        solve(0,digits, temp,mp);
-        return result;
-
+        vector<string> ans;
+        string output="";
+        int idx=0;
+        string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits,output,ans,idx,mapping);
+        return ans;
     }
 };
