92afd763cb61d843735c0f55b08cd750087b08cb
maniksangra2006 <maniksangra2006@gmail.com>
2026-06-19 05:47:36 +0000

    Sync LeetCode submission - Combination Sum III (cpp)

--- problems/combination_sum_iii/solution.cpp
+++ problems/combination_sum_iii/solution.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-  vector<vector<int>> result;
-        vector<int> current;
-void backtrack(int start,int k,int n ){
-      if(n==0&&k==0) {
-    result.push_back(current);
-    return ;
-      }
-    if(k==0||n<0) return;
-
-    for(int i=start;i<=9;i++){
-        current.push_back(i);
-        backtrack(i+1,k-1,n-i );
-        current.pop_back();
+vector<vector<int>> res;
+vector<int> curr;
+    void backtrack(int strt,int k,int n){
+        if(n==0 &&k==0) {
+            res.push_back(curr);
+            return;
+        }
+        if(k==0||n<0 ) return;
+        for(int i=strt;i<=9;i++){
+            curr.push_back(i);
+            backtrack(i+1,k-1,n-i);
+            curr.pop_back();
+        }
     }
-}
-    vector<vector<int>> combinationSum3(int k, int n ) {
-      
-        backtrack(1,k,n );
-        return result;
+    vector<vector<int>> combinationSum3(int k, int n) {
+        backtrack(1,k,n);
+        return res;
     }
 };
