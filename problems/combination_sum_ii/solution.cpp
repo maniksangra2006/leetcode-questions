638b6986d6a119a96ed04338709b0067c369e6da
maniksangra2006 <maniksangra2006@gmail.com>
2026-06-19 05:41:20 +0000

    Sync LeetCode submission - Combination Sum II (cpp)

--- problems/combination_sum_ii/solution.cpp
+++ problems/combination_sum_ii/solution.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-    vector<int> current;
-    void backtrack(vector<int>& candidates, int target,int start){
-        if (target==0){
-            result.push_back(current);
-            return;
-        }
-        
-        for(int i=start;i<candidates.size();i++){
-            if(i>start && candidates[i]==candidates[i-1]) continue;
-            if(candidates[i]>target) break;
-            current.push_back(candidates[i]);
-            backtrack(candidates,target-candidates[i],i+1);
-            current.pop_back();
-        }
-
+vector<vector<int>> res;
+vector<int> curr;
+void backtrack(vector<int>& candidates, int target,int strt){
+    if(target==0){
+ res.push_back(curr);
+ return;
     }
+   
+    for(int i=strt;i<candidates.size();i++){
+           if(i>strt && candidates[i]==candidates[i-1]) continue;
+    if(candidates[i]>target) break;
+        curr.push_back(candidates[i]);
+        backtrack(candidates,target-candidates[i],i+1);
+        curr.pop_back();
+    }
+}
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-         result.clear();
-        current.clear();
+                sort(candidates.begin(),candidates.end());
+
+        res.clear();
+        curr.clear();
         backtrack(candidates,target,0);
-        return result;
+        return res;
     }
 };
