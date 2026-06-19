8c4b8a78ec1729349fbce157d5878342c59068b3
maniksangra2006 <maniksangra2006@gmail.com>
2026-06-19 05:35:57 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- problems/combination_sum/solution.cpp
+++ problems/combination_sum/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
-public: 
-vector<vector<int>> result;
-vector<int> current;
-     void backtrack(vector<int>& candidates, int target,int start){
-        if(target==0){
-            result.push_back(current);
-        }
-        if(target<0) return ;
-
-        for(int i=start;i<candidates.size();i++){
-            
-            current.push_back(candidates[i]);
-            backtrack(candidates,target-candidates[i],i);
-            current.pop_back();
-                    }
-     }
+public:
+vector<vector<int>> res;
+vector<int> curr;
+void backtrack(vector<int>& candidates, int target,int strt){
+    if(target==0) res.push_back(curr);
+    if(target<0) return ;
+    for(int i=strt;i<candidates.size();i++){
+        curr.push_back(candidates[i]);
+        backtrack(candidates,target-candidates[i],i);
+        curr.pop_back();
+    }
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        result.clear();
-        current.clear();
+        res.clear();
+        curr.clear();
         backtrack(candidates,target,0);
-        return result;
+        return res;
     }
 };
